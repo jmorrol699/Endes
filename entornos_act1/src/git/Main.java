d23a1a9182585706c4bf25fadf846f99b7b2f51f
Josemr699 <hensjose26@gmail.com>
2026-02-19 14:18:55 +0100

    Etiqueta Version A

--- entornos_act1/src/git/Main.java
+++ entornos_act1/src/git/Main.java
@@ -7,7 +7,7 @@ public class Main {
 		Calculadora calc = new Calculadora(); 
         int a = 10, b = 3; 
  
-                     String etiqueta = "BASE"; 
+                     String etiqueta = "VERSION A"; 
         System.out.println("Etiqueta: " + etiqueta); 
  
         System.out.println("Suma: " + calc.sumar(a, b)); 
